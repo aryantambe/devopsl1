96fc9647e45aaa6444ce2c355b91d0e4cf285559
PranavReddy065 <138982865+PranavReddy065@users.noreply.github.com>
2025-08-05 13:58:25 +0530

    Add files via upload

---
+++ my-maven-app/src/main/java/com/example/app/App.java
@@ -0,0 +1,17 @@
+package com.example.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Maven Project!" );
+    }
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+}
