3808ee902073fa6d0c98c82a3d288d53fd14a133
Bhargaw Kumar Singh <bhargaw@Bhargaws-MacBook-Air.local>
2025-08-16 18:05:42 +0530

    Added Angular app with Docker and Compose setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Hello from Angular inside Docker!</h1>',
+  styles: []
+})
+export class AppComponent {}
