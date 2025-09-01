88664af43da6fc06cad4fec6f486fee16f37f609
anshsehgal13 <anshsehgal03@gmail.com>
2025-09-02 00:05:12 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,5 @@
+package com.example;
+public class App {
+    public static String greet() { return "Hello, Jenkins!"; }
+    public static void main(String[] args) { System.out.println(greet()); }
+}
---
+++ src/test/java/com/example/AppText.java
@@ -0,0 +1,6 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class AppTest {
+    @Test void testGreet() { assertEquals("Hello, Jenkins!", App.greet()); }
+}
