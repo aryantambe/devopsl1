e2685bcbe2fb59edde2438ecf89820cf1717f02d
Debadrita Nandi <140436189+DebadritaNandi@users.noreply.github.com>
2025-09-02 11:56:41 +0530

    Add files via upload

---
+++ DevOps_L1-assignment2-048/assignment2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Component from './component/component';
+
+function App() {
+  return (
+    <div className="App">
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
