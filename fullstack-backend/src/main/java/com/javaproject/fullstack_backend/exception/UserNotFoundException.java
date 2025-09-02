46b57416c968c13adaa880411c31931fdd4941a3
dhruv-dosh <dhruvdoshi.pvt@gmail.com>
2025-09-02 14:25:07 +0530

    Adding Project

--- fullstack-backend/src/main/java/com/javaproject/fullstack_backend/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.javaproject.fullstack_backend.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
-        super("Invalid Id "+id);
-    }
-}
--- fullstack-backend/src/main/java/com/javaproject/fullstack_backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaproject.fullstack_backend.repository;
-
-import com.javaproject.fullstack_backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-// Hear User is java class in model package and long is datatype of primary key
-public interface UserRepository extends JpaRepository<User,Long>{
-}
